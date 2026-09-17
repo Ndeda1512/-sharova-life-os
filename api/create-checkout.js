61079468298be0c3461f2d9b92516bec7a9658c3
Sharova <sharonvugutsa12@gmail.com>
2026-09-17 06:55:25 +0300

    Use Paystack card channel for USD checkout

--- api/create-checkout.js
+++ api/create-checkout.js
@@ -39,7 +39,8 @@ export default async function handler(req, res) {
           product: 'sharova_life_os',
           plan: 'lifetime'
         },
-        channels: ['card', 'bank', 'mobile_money']
+        // USD checkout is card-based for Kenya.
+        channels: ['card']
       })
     });
 
